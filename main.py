42a8807b6f8c8ffd2180855c2ea5858ac7c3a9ef
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-10-22 19:20:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
 
 def main():
+    # Authenticate and build the Drive API client
+    service = authenticate_drive_api()
 
-  print('hello')
+    # The ID of the folder you want to download files from
+    folder_id = 'YOUR_GOOGLE_DRIVE_FOLDER_ID'  # Replace with your folder ID
+
+    # The directory where you want to save the downloaded files
+    download_dir = 'downloads'
+
+    # Download the files
+    download_files_from_folder(service, folder_id, download_dir)
 
 if __name__ == '__main__':
   main()
