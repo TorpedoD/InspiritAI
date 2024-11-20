fcd4b07a0ea3a9ff4016f5d99bce22112304b946
TorpedoD <81888421+TorpedoD@users.noreply.github.com>
2024-11-19 23:01:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from docx_to_txt import convert_docx_to_txt
 
 def main():
-    input_folder = '/content/Input'  # Use Colab's file system path
+    input_folder = '/content/Input/Docx'  # Use Colab's file system path
     output_folder = '/content/Output'  # Use Colab's file system path
     
     convert_docx_to_txt(input_folder, output_folder)
