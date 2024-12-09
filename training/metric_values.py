4da2c2b5accc2eb9aa3ab3053e5af11ca7d12e4f
TorpedoD <81888421+TorpedoD@users.noreply.github.com>
2024-12-08 21:05:44 -0500

    Update metric_values.py

--- training/metric_values.py
+++ training/metric_values.py
@@ -1,5 +1,5 @@
 import pickle
-from train_llama import TextClassifier  # Import the TextClassifier class from the previous code
+import train_llama
 
 # Define file paths
 model_dir = './saved_model'  # Path to the saved model directory
